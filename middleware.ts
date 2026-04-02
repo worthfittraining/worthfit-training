47b3d15afa8e79bce081c5e9d87aa13e93f2e8f6
Courtney <worthfittraining@gmail.com>
2026-04-01 21:02:33 -0400

    Make playbook-sync webhook public so Zapier can call it without auth

--- middleware.ts
+++ middleware.ts
@@ -7,6 +7,7 @@ const isPublicRoute = createRouteMatcher([
   '/sign-up(.*)',
   '/subscribe(.*)',
   '/terms(.*)',
+  '/api/playbook-sync',
 ])
 
 export default clerkMiddleware(async (auth, request) => {
